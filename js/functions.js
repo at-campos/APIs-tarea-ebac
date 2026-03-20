3713dedbb1bc422ec562be4641ed09b3c17fd45b
at-campos <camposabril01@gmail.com>
2026-03-19 22:51:25 -0600

    basic style css

--- js/functions.js
+++ js/functions.js
@@ -1,1 +1,36 @@
 console.log("Hello show!");
+
+/*
+const createShowcard = (show) => {
+    const card = document.createElement("div");
+    card.classList.add("showcard");
+
+    //contenedor #1
+    const imageContainer = document.createElement("div");
+    imageContainer.classList.add("showcard__image-container");
+
+    const image = document.createElement("img");
+    image.classList.add("showcard__image");
+    image.src = show.image.medium;
+
+    imageContainer.appendChild(image);
+
+    //contenedor #2
+    const dataContainer = document.createElement("div");
+    dataContainer.classList.add("showcard__info");
+
+    const title = document.createElement("h2");
+    title.classList.add("showcard__info-name");
+    title.textContent = show.name;
+
+}*/
+
+document.addEventListener("DOMContentLoaded", () => {
+    axios.get("https://api.tvmaze.com/search/shows", {params: {q: "simpsons"}})
+        .then((response) => {
+            const data = response.data;
+            console.log(data);
+        }) .catch((error) => {
+            console.log(error);
+        })
+});
